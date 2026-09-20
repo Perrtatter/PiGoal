3cf465b5c2524bfdf17004365828299be3795270
Perrtatter <159263635+Perrtatter@users.noreply.github.com>
2026-09-20 14:31:08 +0200

    Update db_backup.sql

--- dev/db_backup.sql
+++ dev/db_backup.sql
@@ -2,12 +2,12 @@
 -- PostgreSQL database dump
 --
 
-\restrict QLWm64kuxOpceuK5tVg5wefk1cLZ8SIO49Y9yjoo9cQO3IjXGCrjXKC01MbVFCu
+\restrict wfqcMfG0HkXmahMaZT2BcFopkr41oNxE2DlU71Ocf2eiKqhGNc6jFYhFfhpEwv6
 
--- Dumped from database version 18.6
--- Dumped by pg_dump version 18.6
+-- Dumped from database version 18.2 (Postgres.app)
+-- Dumped by pg_dump version 18.0
 
--- Started on 2026-09-20 03:35:18
+-- Started on 2026-09-20 14:29:51 CEST
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -26,7 +26,7 @@ SET default_tablespace = '';
 SET default_table_access_method = heap;
 
 --
--- TOC entry 219 (class 1259 OID 24577)
+-- TOC entry 221 (class 1259 OID 16738)
 -- Name: category; Type: TABLE; Schema: public; Owner: postgres
 --
 
@@ -41,7 +41,7 @@ CREATE TABLE public.category (
 ALTER TABLE public.category OWNER TO postgres;
 
 --
--- TOC entry 220 (class 1259 OID 24583)
+-- TOC entry 223 (class 1259 OID 16747)
 -- Name: goal; Type: TABLE; Schema: public; Owner: postgres
 --
 
@@ -56,7 +56,7 @@ CREATE TABLE public.goal (
 ALTER TABLE public.goal OWNER TO postgres;
 
 --
--- TOC entry 221 (class 1259 OID 24591)
+-- TOC entry 222 (class 1259 OID 16746)
 -- Name: goal_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
@@ -72,8 +72,8 @@ CREATE SEQUENCE public.goal_id_seq
 ALTER SEQUENCE public.goal_id_seq OWNER TO postgres;
 
 --
--- TOC entry 5035 (class 0 OID 0)
--- Dependencies: 221
+-- TOC entry 3849 (class 0 OID 0)
+-- Dependencies: 222
 -- Name: goal_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
 
@@ -81,7 +81,7 @@ ALTER SEQUENCE public.goal_id_seq OWNED BY public.goal.id;
 
 
 --
--- TOC entry 222 (class 1259 OID 24592)
+-- TOC entry 220 (class 1259 OID 16728)
 -- Name: user; Type: TABLE; Schema: public; Owner: postgres
 --
 
@@ -96,7 +96,7 @@ CREATE TABLE public."user" (
 ALTER TABLE public."user" OWNER TO postgres;
 
 --
--- TOC entry 223 (class 1259 OID 24600)
+-- TOC entry 219 (class 1259 OID 16727)
 -- Name: user_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
@@ -112,8 +112,8 @@ CREATE SEQUENCE public.user_id_seq
 ALTER SEQUENCE public.user_id_seq OWNER TO postgres;
 
 --
--- TOC entry 5036 (class 0 OID 0)
--- Dependencies: 223
+-- TOC entry 3850 (class 0 OID 0)
+-- Dependencies: 219
 -- Name: user_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
 
@@ -121,7 +121,7 @@ ALTER SEQUENCE public.user_id_seq OWNED BY public."user".id;
 
 
 --
--- TOC entry 4866 (class 2604 OID 24601)
+-- TOC entry 3682 (class 2604 OID 16750)
 -- Name: goal id; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
@@ -129,7 +129,7 @@ ALTER TABLE ONLY public.goal ALTER COLUMN id SET DEFAULT nextval('public.goal_id
 
 
 --
--- TOC entry 4868 (class 2604 OID 24602)
+-- TOC entry 3679 (class 2604 OID 16731)
 -- Name: user id; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
@@ -137,75 +137,67 @@ ALTER TABLE ONLY public."user" ALTER COLUMN id SET DEFAULT nextval('public.user_
 
 
 --
--- TOC entry 5025 (class 0 OID 24577)
--- Dependencies: 219
+-- TOC entry 3841 (class 0 OID 16738)
+-- Dependencies: 221
 -- Data for Name: category; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-COPY public.category (user_id, goal_id, nom, is_complete) FROM stdin;
-1	5	Calestenics	f
-1	6	Calestenics	f
-1	7	Calestenics	f
-1	8	Calestenics	f
-2	9	Gym	f
-2	10	Gym	f
-2	11	Gym	f
-2	12	Gym	f
-1	16	Gym	f
-1	13	Gym	f
-1	14	Gym	f
-1	15	Gym	f
-2	17	Test	t
-\.
+INSERT INTO public.category VALUES (1, 16, 'Gym', false);
+INSERT INTO public.category VALUES (1, 13, 'Gym', false);
+INSERT INTO public.category VALUES (1, 14, 'Gym', false);
+INSERT INTO public.category VALUES (1, 15, 'Gym', false);
+INSERT INTO public.category VALUES (2, 9, 'Gym', false);
+INSERT INTO public.category VALUES (2, 10, 'Gym', false);
+INSERT INTO public.category VALUES (2, 11, 'Gym', false);
+INSERT INTO public.category VALUES (2, 12, 'Gym', false);
+INSERT INTO public.category VALUES (1, 5, 'Calestenics', false);
+INSERT INTO public.category VALUES (1, 6, 'Calestenics', false);
+INSERT INTO public.category VALUES (1, 7, 'Calestenics', false);
+INSERT INTO public.category VALUES (1, 8, 'Calestenics', false);
 
 
 --
--- TOC entry 5026 (class 0 OID 24583)
--- Dependencies: 220
+-- TOC entry 3843 (class 0 OID 16747)
+-- Dependencies: 223
 -- Data for Name: goal; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-COPY public.goal (id, nom, type, is_complete) FROM stdin;
-5	HSPU semi-ampli	4	f
-6	HSPU coude 90°	3	f
-7	HS en elbow	2	f
-8	l-sit en HS	1	f
-13	side	4	f
-14	side sol	3	f
-15	double front	2	f
-16	double back	1	f
-10	backfull	1	t
-9	1/2 backfull	2	t
-11	double side	3	t
-12	double front	4	t
-17	Test Goal id 17	4	t
-\.
+INSERT INTO public.goal VALUES (10, 'backfull', 1, false);
+INSERT INTO public.goal VALUES (6, 'HSPU coude 90°', 3, false);
+INSERT INTO public.goal VALUES (5, 'HSPU semi-ampli', 4, false);
+INSERT INTO public.goal VALUES (8, 'l-sit en HS', 1, false);
+INSERT INTO public.goal VALUES (7, 'HS en elbow', 2, false);
+INSERT INTO public.goal VALUES (14, 'side sol', 3, false);
+INSERT INTO public.goal VALUES (15, 'double front', 2, false);
+INSERT INTO public.goal VALUES (16, 'double back', 1, false);
+INSERT INTO public.goal VALUES (13, 'side', 4, false);
+INSERT INTO public.goal VALUES (9, '1/2 backfull', 2, false);
+INSERT INTO public.goal VALUES (11, 'double side', 3, false);
+INSERT INTO public.goal VALUES (12, 'double front', 4, false);
 
 
 --
--- TOC entry 5028 (class 0 OID 24592)
--- Dependencies: 222
+-- TOC entry 3840 (class 0 OID 16728)
+-- Dependencies: 220
 -- Data for Name: user; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-COPY public."user" (id, username, password_hash, nbr_point) FROM stdin;
-1	Mathys	174dd44f9d8cbed784d676260f13aecd0e04e29f49ca9420645bc45deb649ea7	0
-2	Quentyn	ef260e9aa3c673af240d17a2660480361a8e081d1ffeca2a5ed0e3219fc18567	0
-\.
+INSERT INTO public."user" VALUES (2, 'Quentyn', 'fdaa1733a7999f41e41878fe887843476a512b39b1da20323b221ec7ed4c6ed6', 0);
+INSERT INTO public."user" VALUES (1, 'Mathys', '174dd44f9d8cbed784d676260f13aecd0e04e29f49ca9420645bc45deb649ea7', 0);
 
 
 --
--- TOC entry 5037 (class 0 OID 0)
--- Dependencies: 221
+-- TOC entry 3851 (class 0 OID 0)
+-- Dependencies: 222
 -- Name: goal_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('public.goal_id_seq', 17, true);
+SELECT pg_catalog.setval('public.goal_id_seq', 16, true);
 
 
 --
--- TOC entry 5038 (class 0 OID 0)
--- Dependencies: 223
+-- TOC entry 3852 (class 0 OID 0)
+-- Dependencies: 219
 -- Name: user_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
@@ -213,7 +205,7 @@ SELECT pg_catalog.setval('public.user_id_seq', 2, true);
 
 
 --
--- TOC entry 4871 (class 2606 OID 24604)
+-- TOC entry 3687 (class 2606 OID 16745)
 -- Name: category category_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -222,7 +214,7 @@ ALTER TABLE ONLY public.category
 
 
 --
--- TOC entry 4873 (class 2606 OID 24606)
+-- TOC entry 3689 (class 2606 OID 16756)
 -- Name: goal goal_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -231,7 +223,7 @@ ALTER TABLE ONLY public.goal
 
 
 --
--- TOC entry 4875 (class 2606 OID 24608)
+-- TOC entry 3685 (class 2606 OID 16737)
 -- Name: user user_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -240,7 +232,7 @@ ALTER TABLE ONLY public."user"
 
 
 --
--- TOC entry 4876 (class 2606 OID 24609)
+-- TOC entry 3690 (class 2606 OID 16762)
 -- Name: category fk_category_goal; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -249,7 +241,7 @@ ALTER TABLE ONLY public.category
 
 
 --
--- TOC entry 4877 (class 2606 OID 24614)
+-- TOC entry 3691 (class 2606 OID 16757)
 -- Name: category fk_category_user; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -257,11 +249,11 @@ ALTER TABLE ONLY public.category
     ADD CONSTRAINT fk_category_user FOREIGN KEY (user_id) REFERENCES public."user"(id);
 
 
--- Completed on 2026-09-20 03:35:19
+-- Completed on 2026-09-20 14:29:52 CEST
 
 --
 -- PostgreSQL database dump complete
 --
 
-\unrestrict QLWm64kuxOpceuK5tVg5wefk1cLZ8SIO49Y9yjoo9cQO3IjXGCrjXKC01MbVFCu
+\unrestrict wfqcMfG0HkXmahMaZT2BcFopkr41oNxE2DlU71Ocf2eiKqhGNc6jFYhFfhpEwv6
 
